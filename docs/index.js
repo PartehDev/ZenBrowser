0a53e488783468339dfed72fc2b7ea026af747ed
Parteh <89151213+PartehDev@users.noreply.github.com>
2025-04-05 11:05:23 -0400

    Update website, add release and add requirements.txt

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+function goToDownloads() {
+    location.href = "/downloads"
+}
+
+function goToContact() {
+    location.href = "/contact"
+}
+
+function goToRepo() {
+    location.href = "https://github.com/PartehDev/ZenBrowser/"
+}
