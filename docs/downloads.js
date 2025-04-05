84e1806ad1b9d3a2c89d50cc51c3a7db3244f713
Parteh <89151213+PartehDev@users.noreply.github.com>
2025-04-05 11:16:45 -0400

    Remove website files from Github
    I'm done with Github Pages' bullshit. I'm just done. I'm switching hosting services.

--- docs/downloads.js
+++
@@ -1,7 +0,0 @@
-function downloadNewest() {
-    window.open("https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip", "_blank");
-}
-
-function downloadAlpha() {
-    window.open("https://github.com/PartehDev/ZenBrowser/releases/download/Alpha/ZenBrowser-Alpha-v0.0.1.zip", "_blank");
-}
